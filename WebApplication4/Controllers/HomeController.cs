ce7363657590e7a524dbff7b12c60057e48843b1
KANGDONGHOON0413 <kmerits@gmail.com>
2022-09-22 21:38:18 +0900

    초기 커밋.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Newpage()
+        {
+            return View();
+        }
+
+        public IActionResult ChangedPage()
+        {
+            UserClass newUser01 = new UserClass
+            {
+                UserID = "newbee01",
+                UserSerial = 201984001,
+                UserName = "김민재",
+                UserPhone = "010-0000-0001"
+            };
+            ViewBag.NewUser = newUser01;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication4/Models/UserClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class UserClass
+    {
+        public int UserSerial { get; set; }
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPhone { get; set; }
+    }
+}
